4813af8b39197371f989ea83eb20a76fb0e0f8b5
Chicheng Liu <liuchicheng0401@hotmail.com>
2022-06-01 22:54:25 +0200

    feat: update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
     <div class="grid grid-cols-6 gap-4">
         <div class="col-start-1 col-end-3">
-            <DataRepresentationView />
+            <div class="grid grid-rows-2 grid-flow-col gap-4 h-full">
+                <div>
+                    <ProjectionView />
+                </div>
+                <div>
+                    <TableDataView />
+                </div>
+            </div>
         </div>
         <div class="col-start-3 col-end-7">
             <ObliqueTreeView />
@@ -10,7 +17,8 @@
 </template>
 <script setup>
 import ObliqueTreeView from "../components/ObliqueTreeView/ObliqueTreeView.vue";
-import DataRepresentationView from "../components/DataRepresentationView/DataRepresentationView.vue";
+import ProjectionView from "../components/DataRepresentationView/ProjectionView.vue";
+import TableDataView from "../components/DataRepresentationView/TableDataView.vue";
 
 </script>
 <style scoped>
