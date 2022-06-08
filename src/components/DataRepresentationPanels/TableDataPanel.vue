083807f1fd0a44bd1c2e9df3caee9cbad888b268
Chicheng Liu <liuchicheng0401@hotmail.com>
2022-06-08 20:51:50 +0200

    feat: add antd table in TableData Panel

--- src/components/DataRepresentationPanels/TableDataPanel.vue
+++ src/components/DataRepresentationPanels/TableDataPanel.vue
@@ -1,11 +1,59 @@
 <template>
     <div class="h-full">
-        Table Data Panel
+        <div class="table">
+            <a-table
+                bordered
+                :dataSource="dataSource"
+                :columns="columns"
+                :scroll="{ y: true, x: 'max-content'}"
+                size="small"
+            >
+            </a-table>
+        </div>
     </div>
 </template>
 <script setup>
+import { ref, reactive, toRefs } from "vue";
 
+const state = reactive({
+    dataSource: [
+        {
+        key: '1',
+        name: 'Mike',
+        age: 32,
+        address: '10 Downing Street',
+        },
+        {
+        key: '2',
+        name: 'John',
+        age: 42,
+        address: '10 Downing Street',
+        },
+    ],
+    columns: [
+        {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+        },
+        {
+        title: 'Age',
+        dataIndex: 'age',
+        key: 'age',
+        },
+        {
+        title: 'Address',
+        dataIndex: 'address',
+        key: 'address',
+        },
+    ],
+});
+
+
+let { dataSource, columns } = toRefs(state);
 </script>
 <style scoped>
-    
+    .table {
+        height: fit-content;
+    }
 </style>
