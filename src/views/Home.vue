a05db2c070eb6fe6ef99a4b5c63b482d073e08c6
Chicheng Liu <liuchicheng0401@hotmail.com>
2022-06-08 21:19:11 +0200

    style: adjust layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
 <div class="container m-auto p-2 w-full min-h-screen min-w-full max-w-screen-2xl">
-    <div class="grid grid-cols-6 gap-2 min-h-screen">
+    <div class="grid grid-cols-4 gap-2 min-h-screen">
         <div class="col-start-1 col-end-2">
-            <div class="grid grid-rows-3 grid-flow-col gap-2 h-screen">
-                <div class="grid row-span-2 grid-flow-col gap border border-gray-800/100">
+            <div class="grid grid-rows-2 grid-flow-col gap-2 h-screen">
+                <div class="grid row-span-1 grid-flow-col gap border border-gray-800/100">
                     <ControlPanel />
                 </div>
                 <div class="grid row-span-1 grid-flow-col gap border border-gray-800/100">
@@ -11,14 +11,14 @@
                 </div>
             </div>
         </div>
-        <div class="col-start-2 col-end-6">
+        <div class="col-start-2 col-end-4">
             <div class="border border-gray-800/100 h-screen">
                 <ObliqueTreePanel />
             </div>
         </div>
-        <div class="col-start-6 col-end-7">
-            <div class="grid grid-rows-3 grid-flow-col gap-2 h-screen">
-                <div class="grid row-span-2 grid-flow-col gap border border-gray-800/100">
+        <div class="col-start-4 col-end-5">
+            <div class="grid grid-rows-2 grid-flow-col gap-2 h-screen">
+                <div class="grid row-span-1 grid-flow-col gap border border-gray-800/100">
                     <FeaturePanel />
                 </div>
                 <div class="grid row-span-1 grid-flow-col gap border border-gray-800/100">
