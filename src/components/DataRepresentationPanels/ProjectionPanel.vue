cb78a1197fbbd7a14f1103f5d501e2d04b973c7f
Chicheng Liu <liuchicheng0401@hotmail.com>
2022-06-25 16:24:44 +0200

    feat: add getDataset() api in ProjectionPanel.vu

--- src/components/DataRepresentationPanels/ProjectionPanel.vue
+++ src/components/DataRepresentationPanels/ProjectionPanel.vue
@@ -6,9 +6,28 @@
             </div>
         </div>
         <hr style="height:1px" />
+        <div class="w-full h-screen overflow-auto" id="projection"></div>
     </div>
 </template>
 <script setup>
+import { onMounted, inject, ref } from "vue";
+import { getDataset } from "@/api/dataset.js";
+let d3 = inject("d3");
+
+// const rootNode = ref({});
+// const trainingData = ref({});
+
+onMounted(async() => {
+    await getDataset()
+        .then(function (bundle) {
+            const { trainingSet, labelSet } = bundle.data;
+            console.log(trainingSet, labelSet);
+        }).catch((error) => {
+            console.log("ERROR: ", error);
+        });
+})
+
+
 
 </script>
 <style scoped>
