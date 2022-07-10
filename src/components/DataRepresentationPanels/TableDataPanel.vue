1cdc3c42013ace1480db3e3e7bd9b52febbd438e
Chicheng Liu <liuchicheng0401@hotmail.com>
2022-07-10 17:38:49 +0200

    feat: update dataSource during projection view selection and selected row coloring

--- src/components/DataRepresentationPanels/TableDataPanel.vue
+++ src/components/DataRepresentationPanels/TableDataPanel.vue
@@ -35,6 +35,7 @@ const state = reactive({
     tableHeight: 200,
     tableWidth: 800,
     dataSource: [],
+    trainingSet: [],
     columns: [],
     selectedDataIndexArray: []
 });
@@ -47,7 +48,7 @@ const initializeDataTable = async (dataset_name) => {
     let req = {
         dataset_name: dataset_name
     };
-    state.dataSource = await getDatasetChangeSelects(req)
+    state.trainingSet = await getDatasetChangeSelects(req)
         .then(function (bundle) {
             let { trainingSet, labelSet } = bundle.data;
             trainingSet = dataset_name === "penguins" ? trainingSet.map(([f_1, f_2, f_3, f_4, f_5, f_6, f_7, f_8]) => ({ f_1, f_2, f_3, f_4, f_5, f_6, f_7, f_8 }))
@@ -66,7 +67,8 @@ const initializeDataTable = async (dataset_name) => {
             dataIndex: feature,
             key: feature,
         }));
-    }
+    };
+    state.dataSource = state.trainingSet.map(obj => ({...obj}));
 }
 
 watch(() => props.selectedDataset,
@@ -79,7 +81,12 @@ watch(() => props.selectedDataset,
 watch(() => props.selectedPoints, (newValue, oldValue) => {
     // Update selected data index array
     state.selectedDataIndexArray = newValue.map(point => point.id);
-});
+    state.dataSource = state.trainingSet.filter((obj, index) => state.selectedDataIndexArray.includes(index));
+    if (oldValue.length !== 0 && newValue.length === 0) {
+        state.dataSource = state.trainingSet.map(obj => ({...obj}));
+    }
+}, { immediate: false });
+
 
 const selectedRowsClassed = (record, index) => {
     if (state.selectedDataIndexArray.includes(index)) {
@@ -90,13 +97,13 @@ const selectedRowsClassed = (record, index) => {
 
 let { pagination, tableHeight, tableWidth, dataSource, columns } = toRefs(state);
 </script>
-<style scoped>
+<style>
 .table {
     height: fit-content;
 }
 
 tr.selected-row {
-    border: 1px solid lime;
-    color: blueviolet;
+    /* border: 2px solid lime; */
+    background-color: blueviolet;
 }
 </style>
