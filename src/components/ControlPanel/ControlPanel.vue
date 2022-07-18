5a2ea80c0ae731f1a55649a2510cc8b22cb50aaa
Chicheng Liu <liuchicheng0401@hotmail.com>
2022-07-18 22:48:28 +0200

    fix: remove useless code in ControlPanel

--- src/components/ControlPanel/ControlPanel.vue
+++ src/components/ControlPanel/ControlPanel.vue
@@ -118,7 +118,6 @@ const mouseover = function(d) {
 }
 
 const mousemove = function(event, d) {
-    console.log(d3.pointer(event));
     state.tooltip.html("Count: " + d.count)
         .style("left", (d3.pointer(event)[0]/4) + "px")
         .style("top", (d3.pointer(event)[1]) + "px");
