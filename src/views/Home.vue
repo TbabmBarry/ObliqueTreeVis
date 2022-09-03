3ea538522572250b009d146e3bb0950f945f9c77
Chicheng Liu <liuchicheng0401@hotmail.com>
2022-09-03 11:23:26 +0800

    style: update container background color and view box shadow effect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-<div class="container m-auto p-2 w-full min-h-screen min-w-full max-w-screen-2xl">
+<div class="container bg-slate-200 w-full h-screen min-w-full max-w-screen-2xl overflow-hidden">
     <div class="grid grid-cols-4 gap-2">
         <div class="col-start-1 col-end-2 grid grid-rows-2 grid-flow-col gap-2 h-screen">
-            <div class="grid row-span-1 grid-flow-col gap border-2 border-gray-800/100 rounded-md">
+            <div class="grid row-span-1 grid-flow-col gap bg-white shadow hover:shadow-lg shadow-slate-200/20 rounded-md">
                 <ControlPanel @emit-selected-dataset-changed="selectedDatasetChanged" :selectedPoints="selectedPoints" />
             </div>
-            <div class="grid row-span-1 grid-flow-col gap border-2 border-gray-800/100 rounded-md">
+            <div class="grid row-span-1 grid-flow-col gap bg-white shadow hover:shadow-lg shadow-slate-200/20 rounded-md">
                 <ProjectionPanel @emit-selected-points-changed="selectedPointsChanged" :selectedDataset="selectedDataset" />
             </div>
         </div>
-        <div class="col-start-2 col-end-4 border-2 border-gray-800/100 rounded-md h-screen">
+        <div class="col-start-2 col-end-4 bg-white shadow hover:shadow-lg shadow-slate-200/20 rounded-md h-screen">
             <ObliqueTreePanel :selectedPoints="selectedPoints" :selectedDataset="selectedDataset" />
         </div>
-        <div class="col-start-4 col-end-5 border-2 border-gray-800/100 rounded-md h-screen">
+        <div class="col-start-4 col-end-5 bg-white shadow hover:shadow-lg shadow-slate-200/20 rounded-md h-screen">
             <FeaturePanel />
         </div>
     </div>
