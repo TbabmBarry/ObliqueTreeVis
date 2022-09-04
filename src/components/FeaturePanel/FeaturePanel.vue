fe79080ff718a7a50f484fcd4b67a1a2cbf16006
Chicheng Liu <liuchicheng0401@hotmail.com>
2022-09-04 11:13:55 +0800

    feat: render boxplot inside each table cell

--- src/components/FeaturePanel/FeaturePanel.vue
+++ src/components/FeaturePanel/FeaturePanel.vue
@@ -69,117 +69,15 @@ async function initGlobalFeatureView(dataset_name) {
                 min: d3.min(featureData[feature]),
                 max: d3.max(featureData[feature]),
                 q1: d3.quantile(featureData[feature], 0.25),
-                q2: d3.quantile(featureData[feature], 0.5),
+                median: d3.quantile(featureData[feature], 0.5),
                 q3: d3.quantile(featureData[feature], 0.75),
             }
         })
     });
-    initFeatureTable("penguins");
-    const { width, height } = _.pick(state.rootElement.getBoundingClientRect(), ["width", "height"]);
-    state.width = width;
-    state.height = height*(3/5);
-    // Create the base svg element binding to the root element
-    const baseSvg = d3.select("#feature")
-        .append("svg")
-        .attr("width", state.width)
-        .attr("height", state.height)
-        .attr("class", "feature-view")
-        .attr("id", "feature-view");
-
-    const y = d3.scaleBand()
-        .domain(features)
-        .range([state.height - state.padding, state.padding])
-        .padding(0.1);
-    // Show the y scale
-    baseSvg.append("g")
-        .attr("transform", `translate(${state.padding}, 0)`)
-        .call(d3.axisLeft(y).tickSize(0).tickPadding(10));
-
-    const x = d3.scaleLinear()
-        .domain([state.min, state.max])
-        .range([state.padding, state.width - state.padding]);
-    
-    // Show the x scale
-    baseSvg.append("g")
-        .attr("transform", `translate(0, ${state.height - state.padding})`)
-        .call(d3.axisBottom(x).tickPadding(10));
-
-    // Color scale
-    const myColor = d3.scaleSequential()
-        .interpolator(d3.interpolateInferno)
-        .domain([0,1]);
-
-    // Add the axis labels
-    baseSvg.append("text")
-        .attr("text-anchor", "end")
-        .attr("x", state.width)
-        .attr("y", state.height)
-        .text("Feature Value");
-
-    // Show the main vertical line
-    baseSvg
-        .selectAll("vertLines")
-        .data(state.featureTable)
-        .enter()
-        .append("line")
-            .attr("x1", function(d){ return(x(d.boxplot.min))})
-            .attr("x2", function(d){ return(x(d.boxplot.max))})
-            .attr("y1", function(d){ return(y(d.name)+y.bandwidth()/2)})
-            .attr("y2", function(d){ return(y(d.name)+y.bandwidth()/2)})
-            .attr("stroke", "black")
-            .style("width", 40)
-    
-    // Draw rectangle for the main box
-    baseSvg
-        .selectAll("boxes")
-        .data(state.featureTable)
-        .enter()
-        .append("rect")
-            .attr("x", function(d){ return(x(d.boxplot.q1))})
-            .attr("y", function(d){ return(y(d.name))})
-            .attr("height", function(d){ return(y.bandwidth())})
-            .attr("width", function(d){ return(x(d.boxplot.q3)-x(d.boxplot.q1))})
-            .attr("stroke", "black")
-            .style("fill", "#69b3a2")
-            .style("opacity", 0.5)
-    
-    // Show the median
-    baseSvg
-        .selectAll("medianLines")
-        .data(state.featureTable)
-        .enter()
-        .append("line")
-            .attr("x1", function(d){ return(x(d.boxplot.median))})
-            .attr("x2", function(d){ return(x(d.boxplot.median))})
-            .attr("y1", function(d){ return(y(d.name))})
-            .attr("y2", function(d){ return(y(d.name)+y.bandwidth())})
-            .attr("stroke", "black")
-            .style("width", 80)
-
-    // Create a tooltip
-    let tooltip = d3.select("#feature-tooltip")
-        .append("div")
-        .style("opacity", 0)
-        .attr("class", "tooltip")
-        .style("background-color", "white")
-        .style("border", "solid")
-        .style("border-width", "2px")
-        .style("border-radius", "5px")
-        .style("padding", "5px")
-        .style("font-size", "12px");
-    
-    // Three function that change the tooltip when user hover / move / leave a cell
-    // let mouseover = function(d) {
-    //     tooltip
-    //         .transition()
-    //         .duration(200)
-    //         .style("opacity", 1);
-    //     tooltip
-    //         .html("<span style='color:grey'>Feature Value: </span>" + )
-    // }
+    initFeatureTable();
 }
 
-const initFeatureTable = (featureData) => {
+const initFeatureTable = () => {
     let tableSvg = d3.select(".feature-table");
     // Append a table to the div
     let table = tableSvg.append("table")
@@ -223,28 +121,75 @@ const initFeatureTable = (featureData) => {
         .enter()
         .append("td")
         .attr("class", "border border-slate-300")
-        .text((d) => d)
+        .text((d) => {
+            if (typeof d === "string") return d;
+        })
         .filter((d) => typeof d !== "string")
         .append((d) => {
-            let w = 50, h = 50;
-            let boxplot = document.createElement("div");
-            let boxplotSvg = d3.select(boxplot).append("svg")
-                .attr("width", w)
-                .attr("height", h);
-            
-            let cell = boxplotSvg.selectAll("div")
-                .data([d]);
-            
-            let cellEnter = cell.enter()
-                .append("g")
-                .attr("transform", `translate(${w/2}, ${h/2})`);
-            
-            cellEnter.append("circle")
-                .attr("r", 20)
-                .attr("fill", "red");
-
-            return boxplot;
+            console.log(d);
+            return drawBoxplot(d);
         })
+}
+
+const drawBoxplot = (boxplotData) => {
+    const { width, height } = _.pick(state.rootElement.getBoundingClientRect(), ["width", "height"]);
+    let w = width * 0.8, h = w * 0.2, padding = 10;
+    const features = Object.keys(state.trainingData[0]);
+
+    const x = d3.scaleLinear()
+        .domain([state.min, state.max])
+        .range([padding, w-padding]);
+
+    let boxplot = document.createElement("div");
+    let boxplotSvg = d3.select(boxplot).append("svg")
+        .attr("width", w)
+        .attr("height", h);
+    
+    let cell = boxplotSvg.append("g");
+
+    // Show the x scale
+    cell.append("g")
+        .attr("transform", `translate(0, ${h-padding})`)
+        .call(d3.axisBottom(x).tickPadding(10));
+
+    // Show the main vertical line
+    cell.selectAll("vertLines")
+        .data([boxplotData])
+        .enter()
+        .append("line")
+            .attr("x1", function(d){ return(x(d.min))})
+            .attr("x2", function(d){ return(x(d.max))})
+            .attr("y1", function(d){ return(h/2)})
+            .attr("y2", function(d){ return(h/2)})
+            .attr("stroke", "black")
+            .style("width", 40)
+    
+    // Draw rectangle for the main box
+    cell.selectAll("boxes")
+        .data([boxplotData])
+        .enter()
+        .append("rect")
+            .attr("x", function(d){ return(x(d.q1))})
+            .attr("y", function(d){ return(padding)})
+            .attr("height", function(d){ return(h-2*padding)})
+            .attr("width", function(d){ return(x(d.q3)-x(d.q1))})
+            .attr("stroke", "black")
+            .style("fill", "#69b3a2")
+            .style("opacity", 0.5)
+    
+    // Show the median
+    cell.selectAll("medianLines")
+        .data([boxplotData])
+        .enter()
+        .append("line")
+            .attr("x1", function(d){ return(x(d.median))})
+            .attr("x2", function(d){ return(x(d.median))})
+            .attr("y1", function(d){ return(padding)})
+            .attr("y2", function(d){ return(h-padding)})
+            .attr("stroke", "black")
+            .style("width", 80);
+    
+    return boxplot;
 }
 
 watch(() => props.selectedDataset, (newVal, oldVal) => {
