c524948caca00b24a614de02c987e961b69f0ece
Chicheng Liu <liuchicheng0401@hotmail.com>
2022-09-09 22:58:47 +0800

    feat: enable click the title in control panel and open link in new tab

--- src/components/ControlPanel/ControlPanel.vue
+++ src/components/ControlPanel/ControlPanel.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="m-2 flex flex-col">
         <div class="titles my-2">
-            <a href="https://github.com/TbabmBarry/oblique-tree/tree/main" 
-                class="text-2xl text-black underline decoration-4 decoration-sky-500 hover:underline hover:text-sky-800">ObliqueTreeVis</a>
+            <a 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                href="https://github.com/TbabmBarry/oblique-tree/tree/main" 
+                class="text-2xl text-black underline decoration-4 decoration-sky-500 hover:underline hover:text-sky-800">
+                ObliqueTreeVis
+            </a>
         </div>
         <div class="leading-normal grid grid-cols-2 gap my-2">
             <span class="col-span-1 titles align-bottom">Type: </span>
