0c65128d68ec13d2f38f70c5545e1c24d7c5259c
Chicheng Liu <liuchicheng0401@hotmail.com>
2022-10-01 22:53:24 +0800

    fix: shorten the class name

--- src/libs/ObliqueDecisionTree/ObliqueDecisionTree.js
+++ src/libs/ObliqueDecisionTree/ObliqueDecisionTree.js
@@ -87,7 +87,7 @@ class Odt {
                     "penguins": ['Adelie','Gentoo','Chinstrap'],
                     "iris": ['Setosa', 'Versicolor', 'Virginica'],
                     "wine": ['Class_0', 'Class_1', 'Class_2'],
-                    "blobs": ['Rotundicauda', 'Gigas', 'Tridentatus']
+                    "blobs": ['Rotund', 'Gigas', 'Trident']
                 },
                 featureColorScale: d3.scaleOrdinal(["#4e79a7","#f28e2c","#e15759","#76b7b2","#59a14f","#edc949","#af7aa1","#ff9da7","#9c755f","#bab0ab"]),
                 featureArr: null,
